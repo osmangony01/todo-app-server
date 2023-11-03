80a9756a2f353c83605c79d05b0be62a192cc0ab
osmangony01 <mdosmangoni376@gmail.com>
2023-11-03 14:28:15 +0600

    create a server and connect with mongodb

---
+++ index.js
@@ -0,0 +1,57 @@
+
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+    res.send("task server is running...")
+})
+
+const uri = process.env.DB_URL;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        client.connect();
+        // Send a ping to confirm a successful connection
+
+        const userCollection = client.db('taskMg').collection('users');
+        
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Summar camp is running on PORT : ${PORT}`)
+})
